fe4cadb421c2712f9386ea0a0741ae2822d99b19
varun <VarunSureshkumarPatel07@gmail.com>
2025-12-27 11:35:36 +0530

    added the react-router

--- Frontend/src/constant/constant.js
+++ Frontend/src/constant/constant.js
@@ -3,7 +3,10 @@ export const UNAUTHORIZED_STATUS_CODE = [404, 401, 403];
 export const ERROR_MESSAGES = {
   SOMETHING_WENT_WRONG: "Something went wrong",
   UNAUTHORIZED: "Unauthorized",
-  NO_RESPONSE_FROM_BACKEND:"No response from the backend"
+  NO_RESPONSE_FROM_BACKEND: "No response from the backend",
+  REQUIRED_FIELD: "This is an required field",
+  VALID_EMAIL: "Please enter a valid email",
+  STRONG_PASSWORD: "Please enter a strong password",
 };
 
 export const AUTH_PAGE_PATH = "/auth/sign-in";
